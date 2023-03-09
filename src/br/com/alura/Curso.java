d871ae5d2ceb0585d33b03ea6b6a361e5c6609a6
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-03-09 08:33:16 -0300

    Adicionando arquivos

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,6 +1,5 @@
 package br.com.alura;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,5 +28,9 @@ public class Curso {
 
     public void adiciona(Aula aula) {
         this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
     }
 }
---
+++ src/br/com/alura/TestaCurso2.java
@@ -0,0 +1,26 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando Coleções do Java", "Paulo Silveira");
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando coleções", 24));
+
+        List<Aula> aulasImutavies = javaColecoes.getAulas();
+        System.out.println(aulasImutavies);
+
+        List<Aula> aulas = new ArrayList<>(aulasImutavies);
+
+        Collections.sort(aulas);
+        System.out.println(aulas);
+        System.out.println(javaColecoes.getTempoTotal());
+
+
+    }
+}
