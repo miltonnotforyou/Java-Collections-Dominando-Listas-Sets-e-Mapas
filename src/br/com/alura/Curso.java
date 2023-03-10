3d43ff367cbbcc3b63027d57f974b1a0001b44dc
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-03-10 09:00:11 -0300

    Adicionando arquivos

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,13 +1,12 @@
 package br.com.alura;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -47,5 +46,13 @@ public class Curso {
     @Override
     public String toString() {
        return "[Curso: " + nome + ",tempo total: " + this.getTempoTotal() + "," + "aulas: " + this.aulas + "]";
+    }
+
+    public void matricula(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
     }
 }
